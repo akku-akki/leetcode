760c5822601305cff6b6c7aa48882a7f47417c3f
Ankit Gupta <ankitgupta_@outlook.com>
2022-09-11 20:21:25 +0530

    add solution for duplicates

---
+++ lib/cyclic_sort/find_duplicate.dart
@@ -0,0 +1,23 @@
+void main() {
+  print(findDuplicate([1, 3, 4, 2, 2]));
+}
+
+int findDuplicate(List<int> nums) {
+  int i = 0;
+  while (i < nums.length) {
+    if (nums[i] != i + 1) {
+      int correct = nums[i] - 1;
+      if (nums[i] != nums[correct]) {
+        int temp = nums[correct];
+        nums[correct] = nums[i];
+        nums[i] = temp;
+      } else {
+        return nums[i];
+      }
+    } else {
+      i++;
+    }
+  }
+
+  return -1;
+}
