7deeb72e2fa4e81e372a133df79accd940da6dc7
Ankit Gupta <ankitgupta_@outlook.com>
2022-09-12 22:23:05 +0530

    add solution for first and last position

---
+++ lib/kk/binary_search/first_and_last_position.dart
@@ -0,0 +1,35 @@
+void main() {}
+
+List<int> searchRange(List<int> nums, int target) {
+  List<int> ans = [-1, -1];
+
+  int start = search(nums, target, true);
+  int end = search(nums, target, false);
+  ans[0] = start;
+  ans[1] = end;
+  return ans;
+}
+
+int search(List<int> nums, int target, bool findStartIndex) {
+  int ans = -1;
+  int start = 0;
+  int end = nums.length - 1;
+
+  while (start <= end) {
+    int mid = (start + end) ~/ 2;
+
+    if (target < nums[mid]) {
+      end = mid - 1;
+    } else if (target > nums[mid]) {
+      start = mid + 1;
+    } else {
+      ans = mid;
+      if (findStartIndex) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+  }
+  return ans;
+}
