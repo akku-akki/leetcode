8c3fa67192677015b7b01fefccac7bd5157c31eb
Ankit Gupta <ankitgupta_@outlook.com>
2022-09-13 15:29:03 +0530

    add soln for roated binary search duplicate

--- lib/kk/binary_search/roated_binary_search_duplicates.dart
+++ lib/kk/binary_search/roated_binary_search_duplicates.dart
@@ -1,17 +1,15 @@
-void main() {}
-
-int search(List<int> nums, int target) {
+bool search(List<int> nums, int target) {
   int pivot = findPivot(nums);
   if (pivot == -1) {
-    return binarySearch(nums, target, 0, nums.length - 1);
+    return binarySearch(nums, target, 0, nums.length - 1) == -1 ? false : true;
   }
   if (nums[pivot] == target) {
-    return pivot;
+    return true;
   }
   if (target >= nums[0]) {
-    return binarySearch(nums, target, 0, pivot - 1);
+    return binarySearch(nums, target, 0, pivot - 1)  == -1 ? false : true;
   }
-  return binarySearch(nums, target, pivot + 1, nums.length - 1);
+  return binarySearch(nums, target, pivot + 1, nums.length - 1)  == -1 ? false : true;
 }
 
 int binarySearch(List<int> nums, int target, int start, int end) {
@@ -41,10 +39,20 @@ int findPivot(List<int> nums) {
     if (mid > start && nums[mid] < nums[mid - 1]) {
       return mid - 1;
     }
-    if (nums[mid] <= nums[start]) {
-      end = mid - 1;
-    } else {
+    if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+      if (start < end && nums[start] > nums[start + 1]) {
+        return start;
+      }
+      start++;
+      if (end > start && nums[end] < nums[end - 1]) {
+        return end - 1;
+      }
+      end--;
+    } else if (nums[start] < nums[mid] ||
+        (nums[start] == nums[mid] && nums[mid] > nums[end])) {
       start = mid + 1;
+    } else {
+      end = mid - 1;
     }
   }
   return -1;
