68c1202fbf1d9b99227f70535c36e4fcb5358ff1
Ankit Gupta <ankitgupta_@outlook.com>
2022-09-16 19:36:45 +0530

    add solution for BSsqrt

---
+++ lib/kk/maths/binary_search_sqrt.dart
@@ -0,0 +1,34 @@
+void main() {
+  int n = 40;
+  int p = 3;
+
+  print(sqrt(n, p));
+}
+
+double sqrt(int n, int p) {
+  int s = 0;
+  int e = n;
+  double root = 0.0;
+  while (s <= e) {
+    int m = s + e ~/ 2;
+    print(m);
+    if (m * m == n) {
+      return m.toDouble();
+    }
+    if (m * m > n) {
+      e = m - 1;
+    } else {
+      s = m + 1;
+      root = m.toDouble();
+    }
+  }
+  double incr = 0.1;
+  for (int i = 0; i < p; i++) {
+    while (root * root <= n) {
+      root += incr;
+    }
+    root -= incr;
+    incr /= 10;
+  }
+  return root;
+}
