6cd88a70ecb721f196b79281d669c3ae597da27d
Ankit Gupta <ankitgupta_@outlook.com>
2022-10-09 22:32:21 +0530

    add soln to remove nth number from end

---
+++ lib/kk/linked_list/remove_nth_node_from_end.dart
@@ -0,0 +1,39 @@
+void main() {
+  final node = Solution().removeNthFromEnd(
+    ListNode(
+      1,
+    ),
+    1,
+  );
+
+  ListNode? current = node;
+
+  while (current != null) {
+    print(current.val);
+    current = current.next;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class Solution {
+  ListNode? removeNthFromEnd(ListNode? head, int n) {
+    ListNode? dummy = ListNode(0);
+    dummy.next = head;
+    ListNode? start = dummy;
+    ListNode? end = dummy;
+    for (int i = 0; i <= n; i++) {
+      end = end?.next;
+    }
+    while (end != null) {
+      end = end.next;
+      start = start!.next;
+    }
+    start?.next = start.next?.next;
+    return dummy.next;
+  }
+}
