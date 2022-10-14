adac27e9ca93c3111a372e58474910ea7e2c029f
Ankit Gupta <ankitgupta_@outlook.com>
2022-10-14 23:44:39 +0530

    add solution for maximum width binary tree

--- lib/backtracking/permutation_2.dart
+++ lib/backtracking/permutation_2.dart
@@ -1,3 +1,5 @@
+import '../test/debug_utils.dart/u.dart';
+
 void main() {
   print(Solution().permuteUnique([1, 1, 2]));
 }
@@ -18,21 +20,50 @@ class Solution {
     List<List<int>> allPermutations,
   ) {
     if (current.length == nums.length) {
+      cPrint(output: 'VISITED STATUS => $visited', c: C.red);
       allPermutations.add([...current]);
+      cPrintBg(output: 'ADDING RESULT $current', c: C.purple);
       return;
     }
     for (int i = 0; i < nums.length; i++) {
       if (visited[i] == true) {
+        cPrintBg(output: 'Value of I is $i', c: C.green);
+        cPrintBg(
+            output: 'CONTINUE DUE TO VISITED VISITED  []=> $visited',
+            c: C.green);
         continue;
       }
+      cPrintBg(
+          output: 'I > 0 comparision I==$i is greaterThan ZERO 00',
+          c: C.yellow);
       if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == false) {
+        cPrintBg(
+            output: '       START     SKIP DUPLICATE                   ',
+            c: C.yellow);
+        cPrintBg(output: 'INDEX => $i', c: C.cyan);
+        cPrintBg(output: 'NUMS => $nums VISITED => $visited ', c: C.cyan);
+        cPrintBg(
+            output: '       END     SKIP DUPLICATE                   ',
+            c: C.yellow);
         continue;
       }
       current.add(nums[i]);
       visited[i] = true;
+      cPrintBg(
+          output:
+              'ADDING AND MARK VISITED AT INDEX $i per => $current VISITED => $visited',
+          c: C.blue);
       permuteInternal(visited, nums, current, allPermutations);
+      cPrintBg(
+          output:
+              'BEFORE REMOVE AND MARK UNVISIT AT INDEX $i per => $current VISITED => $visited',
+          c: C.red);
       visited[i] = false;
       current.removeLast();
+      cPrintBg(
+          output:
+              'REMOVE AND MARK UNVISIT AT INDEX $i per => $current VISITED => $visited',
+          c: C.red);
     }
   }
 }
--- lib/bfs/maximum_width_of_binary_tree.dart
+++ lib/bfs/maximum_width_of_binary_tree.dart
@@ -1,37 +1,41 @@
 import 'dart:collection' show DoubleLinkedQueue;
+import 'dart:math';
 
 void main() {
   TreeNode? node = TreeNode(
     1,
-    TreeNode(
-      3,
-      TreeNode(5, TreeNode(6)),
-    ),
-    TreeNode(
-      2,
-      null,
-      TreeNode(9, TreeNode(7)),
-    ),
+    TreeNode(3, TreeNode(5), TreeNode(3)),
+    TreeNode(2, null, TreeNode(9)),
   );
   print(Solution().widthOfBinaryTree(node));
 }
 
 class Solution {
   int widthOfBinaryTree(TreeNode? root) {
-    DoubleLinkedQueue<TreeNode?> queue = DoubleLinkedQueue();
-    queue.addLast(root!);
+    if (root == null || root.left == null && root.right == null) {
+      return 1;
+    }
+    Map<TreeNode, int> map = {};
+    map[root] = 0;
+    DoubleLinkedQueue<TreeNode> queue = DoubleLinkedQueue();
+    queue.addLast(root);
     int maxWidth = -1;
     while (queue.isNotEmpty) {
       int size = queue.length;
-      maxWidth = maxWidth < size ? size : maxWidth;
+      int left = map[queue.first]!;
       while (size > 0) {
-        TreeNode? current = queue.removeFirst();
-        queue.addLast(current?.left);
-        queue.addLast(current?.right);
+        TreeNode current = queue.removeFirst();
+        maxWidth = max(maxWidth, map[current]! - left + 1);
+        if (current.left != null) {
+          queue.addLast(current.left!);
+          map[current.left!] = 2 * (map[current]!);
+        }
+        if (current.right != null) {
+          queue.addLast(current.right!);
+          map[current.right!] = (2 * map[current]!) + 1;
+        }
         size--;
       }
-      while (queue.isNotEmpty && queue.first == null) queue.removeFirst();
-      while (queue.isNotEmpty && queue.last == null) queue.removeLast();
     }
     return maxWidth;
   }
