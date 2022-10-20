b884f0d3df8036d7d54d614e0069aad6f20baae7
Ankit Gupta <ankitgupta_@outlook.com>
2022-10-20 13:43:22 +0530

    add solution for flood fill

---
+++ lib/matrix/flood_fill.dart
@@ -0,0 +1,59 @@
+void main() {
+  print(
+    Solution().floodFill([
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 0, 1]
+    ], 1, 1, 2),
+  );
+}
+
+class Solution {
+  List<List<int>> floodFill(
+    List<List<int>> image,
+    int sr,
+    int sc,
+    int color,
+  ) {
+    int m = image.length;
+    int n = image[0].length;
+    int oldColor = image[sr][sc];
+    if (oldColor == color) return image;
+    _floodFill(sr, sc, m, n, oldColor, color, image);
+    return image;
+  }
+
+  void _floodFill(
+    int i,
+    int j,
+    int m,
+    int n,
+    int oldColor,
+    int newColor,
+    List<List<int>> image,
+  ) {
+    image[i][j] = newColor;
+    if (isValid(i + 1, j, m, n, oldColor, image))
+      _floodFill(i + 1, j, m, n, oldColor, newColor, image);
+    if (isValid(i - 1, j, m, n, oldColor, image))
+      _floodFill(i - 1, j, m, n, oldColor, newColor, image);
+    if (isValid(i, j + 1, m, n, oldColor, image))
+      _floodFill(i, j + 1, m, n, oldColor, newColor, image);
+    if (isValid(i, j - 1, m, n, oldColor, image))
+      _floodFill(i, j - 1, m, n, oldColor, newColor, image);
+  }
+
+  bool isValid(
+    int i,
+    int j,
+    int m,
+    int n,
+    int oldColor,
+    List<List<int>> image,
+  ) {
+    if (i >= 0 && i < m && j >= 0 && j < n && image[i][j] == oldColor) {
+      return true;
+    }
+    return false;
+  }
+}
